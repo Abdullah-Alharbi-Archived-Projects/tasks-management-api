aaa67acc295c0d5d725454eff635649dd30afd25
W2AlharbiMe <w2.alharbi.me@gmail.com>
2020-05-08 15:17:07 +0300

    created the first route

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  // Nest Dependency Injection
+  constructor(private readonly tasksService: TasksService) {}
+
+  // GET /tasks/
+  @Get()
+  index() {
+    // this will call getTasks from the TasksService
+    return this.tasksService.getTasks();
+  }
+}
